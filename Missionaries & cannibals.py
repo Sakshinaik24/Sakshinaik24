f60225751f8ec1a77068f5a4c8548555981c4b8a
Sakshi Naik <159618971+Sakshinaik24@users.noreply.github.com>
2024-08-02 18:28:49 +0530

    Update Missionaries & cannibals.py

--- Missionaries & cannibals.py
+++ Missionaries & cannibals.py
@@ -1,5 +1,5 @@
 #Python program to illustrate Missionaries & cannibals Problem
-#This code is contributed by Sunit Mal
+
 print("\n")
 print("\tGame Start\nNow the task is to move all of them to right side of the river")
 print("rules:\n1. The boat can carry at most two people\n2. If cannibals num greater than missionaries then the cannibals would eat the missionaries\n3. The boat cannot cross the river by itself with no people on board")
