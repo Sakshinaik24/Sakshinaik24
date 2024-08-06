72c406d67e94ef827fa26a7f22c074e7e7fddfa6
Sakshi Naik <159618971+Sakshinaik24@users.noreply.github.com>
2024-08-06 21:08:16 +0530

    Create positive or negative.c
    positive or negative number

---
+++ positive or negative.c
@@ -0,0 +1,22 @@
+void main ()
+{ int a;
+printf("Enter a number:");
+scanf("%d", &a);
+if (a>0)
+{
+
+    printf(" is positive number:",a);
+
+}
+else if(a<0)
+    {
+        printf(" is negative number:",a);
+    }
+    else if (a==0)
+    {
+        printf(" is neither positive or nor negative number:",a);
+
+    }
+
+
+}
