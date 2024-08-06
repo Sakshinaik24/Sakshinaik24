1e747a91242f68b31a417a813c7d70a047f69a43
Sakshi Naik <159618971+Sakshinaik24@users.noreply.github.com>
2024-08-06 21:10:01 +0530

    Create temperature. c
    temperature calculate

---
+++ temperature.c
@@ -0,0 +1,9 @@
+void main()
+{
+     float c,f;
+     printf("enter a temperature in celcius : ");
+     scanf("%f",&c);
+     f=(c*1.8)+32;
+     printf("temperature  in fahrenheit is:%f ",f);
+
+}
