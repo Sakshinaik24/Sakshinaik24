5e5970c404082f4b4e027b23bab5200ca97a15b1
Sakshi Naik <159618971+Sakshinaik24@users.noreply.github.com>
2024-08-11 17:02:45 +0530

    Create pattern.c
    pattern  
     *
     **
     ***
     ****

---
+++ pattern.c
@@ -0,0 +1,24 @@
+void main()
+
+{
+int i,j;
+
+    for(i=1;i<=4;i++)
+    {
+        for(j=1;j<=4.;j++)
+        {
+            if(j>=5-i&&j<=3+i)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+
+
