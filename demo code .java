04c21791efbb2c0d8ae751ac8188825e524a32be
Sakshi Naik <159618971+Sakshinaik24@users.noreply.github.com>
2024-08-13 22:35:18 +0530

    Create demo code .java
    simple demo code in java

---
+++ demo code .java
@@ -0,0 +1,17 @@
+class Demo1
+{
+	int A, B;
+	void accept()
+	{
+		System.out.println(A);
+	}
+}
+
+
+public class Demo {
+    public static void main(String[] args) {
+        Demo1 d1 = new Demo1();
+        d1.A = 10;
+        d1.accept();
+    }
+}
